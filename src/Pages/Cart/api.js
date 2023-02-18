37e7cc425258821ca44a5389a8c1cfb65498176e
akshay kumar <akshay123332@gmail.com>
2023-02-18 22:23:19 +0530

    git init

--- src/Pages/Cart/api.js
+++
@@ -1,8 +0,0 @@
-
-import  axios  from 'axios';
-
-
-
-export const deleteProduct=async(id)=>{
-    await axios.delete(`https://rich-gray-scarab-fez.cyclic.app/cart/${id}`)
-  }
--- src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
-import { reducer as menspagereducer  } from "./menspage/reducer";
-
-export const store=legacy_createStore(menspagereducer,applyMiddleware(thunk)) 
